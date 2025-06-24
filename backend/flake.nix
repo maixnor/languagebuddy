360cc7b42c3d068733a8fe8a2d74fb71ec1c6c9a
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2025-06-24 04:32:21 +0200

    we got a build

---
+++ backend/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "JavaScript example flake for Zero to Nix";
+
+  inputs = {
+    # Latest stable Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildNpmPackage {
+          name = "languagebuddy";
+
+          buildInputs = with pkgs; [
+            nodejs_24
+          ];
+
+          src = self;
+
+          npmDeps = pkgs.importNpmLock {
+            npmRoot = ./.;
+          };
+
+          npmConfigHook = pkgs.importNpmLock.npmConfigHook;
+
+          installPhase = ''
+            mkdir $out
+            cp dist/src/* -r $out
+          '';
+        };
+      });
+    };
+}
