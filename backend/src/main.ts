a9827ab03f4f855d5152e0dbec2febe5ccb3256c
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2025-07-26 16:39:49 +0200

    ingore empty requests

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -111,9 +111,7 @@ app.post("/webhook", async (req: any, res: any) => {
   }
 
   if (!message || !message.from) {
-    logger.info(message);
-    logger.error("Invalid message format received in webhook.");
-    return res.sendStatus(400);
+    return res.sendStatus(200);
   }
 
   if (message?.type !== "text") {
