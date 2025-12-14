c54a97851cc567f89f04fa0afe4e4f17a72e1ad3
maixnor <46966993+maixnor@users.noreply.github.com>
2025-12-14 16:11:47 -0500

    index page so much better

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   base: '/',
+  vite: {
+    server: {
+      allowedHosts: ['adhoc.maixnor.com'],
+    },
+  },
 });
