f99971444ad28660b6a568d12565a57e00cd952a
maixnor <46966993+maixnor@users.noreply.github.com>
2025-12-26 11:22:25 +0100

    eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    // Add any specific rules or overrides here
+  },
+};
